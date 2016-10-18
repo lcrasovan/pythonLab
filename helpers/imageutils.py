e9aff2b9636e5ae8fae018db6a294fb43ea3f873
lucian crasovan <l.crasovan@resto-in.com>
2016-10-18 18:08:25 +0200

    resize + crop implemented

--- helpers/imageutils.py
+++ helpers/imageutils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib
-from PIL import ImageFile
+from PIL import ImageFile, Image, ImageOps
+import os
 
 class ImageUtils:
     
@@ -30,3 +31,20 @@ class ImageUtils:
         with open(filePath, "a") as myfile:
             myfile.write(imageData)
             return 1
+            
+    def createCrop(self, srcImagePath, destImagePath, zoomLevel, width, height):
+        originalImg = Image.open(srcImagePath)
+        halfOriginaWidth = originalImg.size[0] / 2
+        halfOriginalHeight = originalImg.size[1] / 2
+        tempImage = originalImg.crop((halfOriginaWidth - width/2 * zoomLevel, halfOriginalHeight - height/2 * zoomLevel, halfOriginaWidth + width/2 * zoomLevel, halfOriginalHeight + height/2 * zoomLevel))
+        thumbnail = ImageOps.fit(tempImage,(width, height),Image.ANTIALIAS)
+        thumbnail.save(destImagePath)
+        return 1
+       
+    def createZoomedImages(self, rootDir, srcImagePath):
+        for x in xrange(1,5):
+            zoomLevel = x
+            imageName = os.path.basename(srcImagePath)
+            destImagePath = rootDir + '/images/zoom' + str(zoomLevel) + '/' + imageName 
+            self.createCrop(srcImagePath, destImagePath, zoomLevel, 280, 186) 
+        return 1      
